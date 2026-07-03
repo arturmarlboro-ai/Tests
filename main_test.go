f0db5a9d309531a3808ca066f3ec6447dc9c003f
arturmarlboro-ai <arturmarlboro@gmail.com>
2026-07-03 13:19:25 -0700

    third commit

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strconv"
 	"strings"
 	"testing"
@@ -52,59 +53,81 @@ func TestCafeWhenOk(t *testing.T) {
 }
 func TestCafeCount(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
-	counts := []string{
-		"count=0",
-		"count=1",
-		"count=2",
-		"count=100",
+	//
+	requests := []struct {
+		count int // передаваемое значение count
+		want  int // ожидаемое количество кафе в ответе
+	}{
+		{count: 0, want: 0},
+		{count: 1, want: 1},
+		{count: 2, want: 2},
+		{count: 100, want: 5}, // в Москве 5 кафе
 	}
+
 	cities := []string{"moscow", "tula"}
-	for _, v := range counts {
+	for _, v := range requests {
 		for _, c := range cities {
+			params := url.Values{}
+			if v.count == 0 {
+				params.Set("count", "0")
+			}
+			params.Set("count", strconv.Itoa(v.count))
+			if c != "" {
+				params.Set("city", c)
+			}
+
+			req := httptest.NewRequest("GET", "/cafe?"+params.Encode(), nil)
 			response := httptest.NewRecorder()
-			req := httptest.NewRequest("GET", "/cafe?"+v+"&city="+c, nil)
 
 			handler.ServeHTTP(response, req)
+
 			countStr := req.FormValue("count")
 			count, err := strconv.Atoi(countStr)
 			if err != nil {
 				t.Error(err)
 			}
 			city := req.FormValue("city")
-			cafe := cafeList[city]
-			// if !ok {
-			// 	t.Error(err)
-			// }
-			count = min(count, len(cafe))
-			answer := strings.Join(cafe[:count], ",")
-			//fmt.Printf("Request: %s, City: %s, Count: %d, Expected Answer: %s\n", v, city, count, answer)
+			cafes := cafeList[city]
+
+			count = min(count, len(cafes))
+			answer := strings.Join(cafes[:count], ",")
+
 			answerSlice := strings.Split(answer, ",")
+
 			if len(answerSlice) == 1 && answerSlice[0] == "" {
 				answerSlice = []string{}
 			}
-			require.Equal(t, http.StatusOK, response.Code)
-
 			lenAnswer := len(answerSlice)
-			assert.Equal(t, count, lenAnswer)
+			if city == "tula" && v.count == 100 {
+				v.want = 3
+			}
+			require.Equal(t, http.StatusOK, response.Code)
+			assert.Equal(t, v.want, lenAnswer)
 		}
 	}
 }
 func TestCafeSearch(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
-	c := "city=moscow"
-	look4 := []struct {
-		search    string
-		wantCount int
+	requests := []struct {
+		search    string // передаваемое значение search
+		wantCount int    // ожидаемое количество кафе в ответе
 	}{
-		{"search=фасоль", 0},
-		{"search=кофе", 2},
-		{"search=вилка", 1},
+		{search: "фасоль", wantCount: 0},
+		{search: "кофе", wantCount: 2},
+		{search: "вилка", wantCount: 1},
 	}
+	c := "moscow"
 
-	for _, r := range look4 {
+	for _, r := range requests {
+		params := url.Values{}
+		params.Set("city", c)
+		params.Set("search", r.search)
+
+		req := httptest.NewRequest("GET", "/cafe?"+params.Encode(), nil)
 		response := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", "/cafe?"+c+"&"+r.search, nil)
+
 		handler.ServeHTTP(response, req)
+
 		city := req.FormValue("city")
 		search := req.FormValue("search")
 		cafes := cafeList[city]
